e21f490eb75c609047c9704a895e99acc4aa6d74
Prabhu <prabhusivakumar20@gmail.com>
2022-06-25 21:59:31 +0530

    CheckPoint 1: Implemented Get all Vehicles End Point

---
+++ project/src/main/java/com/upgrad/hirewheels/controller/VehicleController.java
@@ -0,0 +1,41 @@
+package com.upgrad.hirewheels.controller;
+
+import com.upgrad.hirewheels.dto.VehicleDTO;
+import com.upgrad.hirewheels.entities.Vehicle;
+import com.upgrad.hirewheels.services.AdminService;
+import com.upgrad.hirewheels.services.VehicleService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/hirewheels/v1")
+public class VehicleController {
+
+    @Autowired
+    private AdminService adminService;
+
+    @Autowired
+    private VehicleService vehicleService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @GetMapping(value = "/vehicles", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<List<VehicleDTO>> getVehicles() {
+        List<Vehicle> vehicleList = this.vehicleService.getAllVehicles();
+        List<VehicleDTO> vehicleDTOList = Optional.ofNullable(vehicleList).map(vehicles -> vehicles.stream()
+                .map(vehicle -> this.modelMapper.map(vehicle, VehicleDTO.class))
+                .collect(Collectors.toList())).orElse(Collections.emptyList());
+        return ResponseEntity.ok(vehicleDTOList);
+    }
+}
